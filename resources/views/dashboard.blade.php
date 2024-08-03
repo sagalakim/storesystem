3022c34f4ca3d8c9a10b59c462148eede896c8fd
sagalakim <kimjohn.sagala02@gmail.com>
2024-08-03 22:59:12 +0800

    test

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -49,7 +49,7 @@
                             Retail Price
                         </label>
                         <input class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded mb-2 py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="grid-last-name" name='retail' type="number"  placeholder="0"required>
-                        </div>
+                        </div> 
                         
                     </div>
                     </form>
